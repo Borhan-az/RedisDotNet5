7ceb934aacc141c87fc31714b450da425a8fc9a5
borhan azhdari <borhanaz1379@gmail.com>
2021-06-18 02:10:42 +0430

    Pub and sub in Redis

---
+++ ServiceApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------");
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            var getSub = redis.GetSubscriber();
+            getSub.Subscribe("Helloservice", (channel,name)=> Message(name));
+            Console.ReadLine();
+        }
+        private static void Message(RedisValue name) => Console.WriteLine($"Hello {name}");
+
+    }
+}
---
+++ sampleProjectUsingRedis/Controllers/PubController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampleProjectUsingRedis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PubController : ControllerBase
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">
+        /// value that set in Channel 
+        ///</param>
+        /// <returns></returns>
+        public async Task<IActionResult> publisher(string name)
+        {
+            var redis = StackExchange.Redis.ConnectionMultiplexer.Connect("localhost:6379");
+            var pubChannel = redis.GetSubscriber();
+            pubChannel.Publish("Helloservice", name);
+            return Ok();
+        }
+    }
+}
